59dba31aeae8407758674211904aa54ae6c685d9
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-21 04:56:07 -0700

    module import

---
+++ day 2/app.js
@@ -0,0 +1,12 @@
+const data = require("./xyz")
+const {myFunc,x} = require("./sum")
+
+const myName = "Arish"
+const a = 10710
+const b = 208292
+
+myFunc(a,b)
+
+console.log(myName)
+console.log(x)
+// console.log(a+b)
---
+++ day 2/sum.js
@@ -0,0 +1,10 @@
+console.log("Inside Sum Module")
+
+let x = "Hello Cosmo Boss"
+
+function myFunc(a,b){
+    let result = a + b
+    console.log(result)
+}
+
+module.exports = {myFunc,x}
