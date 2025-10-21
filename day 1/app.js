601c7fff78a1faaf58409e52af9d2c4ea90243e0
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-21 04:38:02 -0700

    Basics

---
+++ day 1/app.js
@@ -0,0 +1,12 @@
+let fName = "Usman"
+let lName = "Zaheen"
+
+let a = 10
+let b = 20
+
+console.log(fName + lName)
+console.log(a+b)
+// console.log(global)
+// console.log(this)
+// console.log(globalThis)
+console.log(global === globalThis)
