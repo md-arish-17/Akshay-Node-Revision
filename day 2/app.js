349f05d8c411949da873f83d3c95679fd12058c1
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-21 05:07:51 -0700

    Data.json Import

--- day 2/app.js
+++ day 2/app.js
@@ -1,5 +1,6 @@
-const data = require("./xyz")
+const abc = require("./xyz")
 const {myFunc,myFunc2} = require("./calculate/calculation")
+const data = require("./data.json")
 
 const myName = "Arish"
 const a = 10
@@ -9,4 +10,5 @@ myFunc(a,b)
 myFunc2(a,b)
 
 console.log(myName)
+console.log(data)
 // console.log(a+b)
